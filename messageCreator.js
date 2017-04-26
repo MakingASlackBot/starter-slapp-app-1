dd3d8c0b850cd5930223590843ca78a84adaa594
Martin Stuart <stuartconcept@gmail.com>
2017-04-26 14:56:38 -0500

    i aint no caller back girl

--- messageCreator.js
+++ messageCreator.js
@@ -8,8 +8,8 @@ messageCreator.prototype.getData = function(name, functionName) {
 	var options = {  	
 		//url: https://jira.praeses.com/rest/api/2/search?jql=labels%20in%20(heather.rc%2C%20heather.uat%2C%20heather.qa)
 		//url: 'https://jira.praeses.com/rest/api/2/search?jql=label=' + name,
-		//url: 'https://jira.praeses.com/rest/api/2/search?jql=labels%20%3D%20heather%20and%20status%20%3D%20"In%20Dev"',
-		url: 'https://jira.praeses.com/rest/api/2/search?jql=assignee=mstuart',  //alternate URL for testing
+		url: 'https://jira.praeses.com/rest/api/2/search?jql=labels%20%3D%20' + name + '%20and%20status%20%3D%20"In%20Dev"',
+		//url: 'https://jira.praeses.com/rest/api/2/search?jql=assignee=mstuart',  //alternate URL for testing
 		method: 'GET',
 		headers: {
 			 'Authorization' : 'Basic am9zY2llbmNlZmFpcnRlc3R1c2VyOm5oRGFnMixlS0Q9Vk0zKlU=',
