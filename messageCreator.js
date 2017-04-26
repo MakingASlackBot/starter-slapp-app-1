5191894c0728752b2ab2058cfa664185bca162ab
Natalie Lockhart <natalie.lockhart@praeses.com>
2017-04-26 10:22:33 -0500

    Updating getData function in messageCreator

--- messageCreator.js
+++ messageCreator.js
@@ -2,7 +2,7 @@ var messageCreator = function () {}
 
 const request = require('request')
 
-messageCreator.prototype.getData = function(name) {
+function getData(name) {
 	//todo - make api call with parameters here.
 	
 	var options = {  	
