e4d9a94aedbc5afaeaba12ad4393617d68573a2a
Martin Stuart <stuartconcept@gmail.com>
2017-04-26 14:09:39 -0500

    JSON is stupid

--- messageCreator.js
+++ messageCreator.js
@@ -38,7 +38,7 @@ function TicketFormat(data) {
 	
 	//todo: foreach	
 	//todo: get json object via getData function				
-	console.log("Log 3: " + data[issues]);
+	console.log("Log 3: " + data[0].issues.length);
 	return;
 
 	var MyTicketArray = [];
